e6b9f50136a08b2a6eba4653a4119b809cc4b9e7
Pedro Paulo <pedropaulo062011@gmail.com>
2025-10-08 17:10:33 -0300

    :tada: First commit

---
+++ backend/src/main/java/webii/lap/CardeasyPlus/CardeasyPlusApplication.java
@@ -0,0 +1,13 @@
+package webii.lap.CardeasyPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardeasyPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardeasyPlusApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/webii/lap/CardeasyPlus/CardeasyPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package webii.lap.CardeasyPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardeasyPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
