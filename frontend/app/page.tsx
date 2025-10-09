ec861929876d350a859244f076e0109b4539c614
L-Marcel <lmgh1312@gmail.com>
2025-10-09 19:10:37 -0300

    [feat] Adicionando Daisy UI e exemplo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,18 @@
+import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className={clsx(
+      "font-sans grid grid-rows-[20px_1fr_20px]",
+      "items-center justify-items-center min-h-screen",
+      "p-8 pb-20 gap-16 sm:p-20"
+    )}>
+      <main className={clsx(
+        "flex flex-col gap-[32px] row-start-2",
+        "items-center sm:items-start"
+      )}>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,25 +21,57 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+        <ol className={clsx(
+          "font-mono list-inside list-decimal",
+          "text-sm/6 text-center sm:text-left"
+        )}>
+          <li className="tracking-[-.01em]">
+            Editei essa página para guiar vocês.
+          </li>
+          <li className="tracking-[-.01em]">
+            Experimenta editar{" "}
+            <code className={clsx(
+              "bg-black/[.05] dark:bg-white/[.06]",
+              "font-mono font-semibold px-1 py-0.5 rounded"
+            )}>
               app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Salve e veja as alterações serem renderizadas.
+          </li>
+          <li className="tracking-[-.01em]">
+            Essa página não está usando o <code className={clsx(
+              "bg-black/[.05] dark:bg-white/[.06]",
+              "font-mono font-semibold px-1 py-0.5 rounded"
+            )}>
+              Daisy UI
+            </code> ainda.
+          </li>
+          <li className="tracking-[-.01em]">
+            Eu apenas aproveitei o template inicial.
+          </li>
+          <li className="tracking-[-.01em]">
+            Clique em <code className={clsx(
+              "bg-black/[.05] dark:bg-white/[.06]",
+              "font-mono font-semibold px-1 py-0.5 rounded"
+            )}>
+              próximo
+            </code> para ver o exemplo que deixei para vocês.
           </li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Link
+            className={clsx(
+              "rounded-full border border-solid border-transparent",
+              "transition-colors flex items-center justify-center",
+              "bg-foreground text-background gap-2",
+              "hover:bg-[#383838] dark:hover:bg-[#ccc]", 
+              "font-medium text-sm sm:text-base h-10 sm:h-12", 
+              "px-4 sm:px-5 sm:w-auto"
+            )}
+            href="/teams/ca85079e-978f-4b95-8e73-04b09282fce6"
           >
             <Image
               className="dark:invert"
@@ -39,65 +80,24 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
+            Próximo
+          </Link>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            className={clsx(
+              "rounded-full border border-solid border-black/[.08]dark:border-white/[.145]", 
+              "transition-colors flex items-center justify-center", 
+              "hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a]", 
+              "hover:border-transparent font-medium text-sm sm:text-base",
+              "h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[236px]"
+            )}
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Documentação do Next.js
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 };
